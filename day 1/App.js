11de2ef1d74c0d2eb1897e0086571dd57c855a55
Harsh  Gupta <harshgupta9175478116@gmail.com>
2024-12-04 18:33:04 +0530

    01 episode

---
+++ day 1/App.js
@@ -0,0 +1,4 @@
+const headerr = React.createElement("h1", {id:"header"}, "This is from react");
+
+      const root = ReactDOM.createRoot(document.getElementById("root"));
+      root.render(headerr);
